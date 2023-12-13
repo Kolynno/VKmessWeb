b321aee16f6fd9331978d16df2fd3a8ea336037e
Nickolay Zhmakin <nickzhmakin@mail.ru>
2023-12-13 13:55:42 +0300

    init

---
+++ src/main/java/itmo/nick/vkmessweb/VKmessWebApplication.java
@@ -0,0 +1,13 @@
+package itmo.nick.vkmessweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VKmessWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VKmessWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/itmo/nick/vkmessweb/VKmessWebApplicationTests.java
@@ -0,0 +1,13 @@
+package itmo.nick.vkmessweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VKmessWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
